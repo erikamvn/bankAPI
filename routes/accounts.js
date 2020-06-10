3ce11649774a8cf4ee8c0bfbf18f549fe84c4ad6
Erika Moraes Vaz Nascimento <erikamoraesv@gmail.com>
2020-06-10 12:28:17 -0300

    Delete action + Try/Catch
    Delete action + Try/Catch

--- routes/accounts.js
+++ routes/accounts.js
@@ -6,8 +6,9 @@ router.post("/", (req, res) =>{
     let account = req.body;
 
     fs.readFile(fileName, "utf8", (err, data) =>{
-        if (!err){
-            try{
+        
+        try{
+                
                 let json = JSON.parse(data);
                 account = { id: json.nextId, ...account };
                 json.nextId ++;
@@ -23,22 +24,19 @@ router.post("/", (req, res) =>{
             } catch (err){
                 res.status(400).send({ error: err.menssage });    
             }
-            
-        }else {
-            res.status(400).send({ error: err.menssage });
-        }
     });
 });
 
 router.get("/", (_, res) =>{
     fs.readFile(fileName, "utf8", (err, data) =>{
-        if(!err){
+        try{
+            if(err) throw err;
             let json = JSON.parse(data);
             delete json.nextId;
-        
+            
             res.send(json);
-        }else {
-            res.status(400).send({ error: err.message });
+        }catch (err){
+            res.status(400).send({ error: err.message });            
         }
     });
 });
@@ -47,7 +45,8 @@ router.get("/:id", (req, res) => {
 
         fs.readFile(fileName, "utf8", (err, data) =>{
             try{
-                if(!err){
+                if(err) throw err;
+
                     let json = JSON.parse(data);
                     const account = json.accounts.find(account => account.id === parseInt(req.params.id));
                     
@@ -56,13 +55,32 @@ router.get("/:id", (req, res) => {
                     }else{
                         res.end();
                     }
-                }else {
-                    res.status(400).send({ error: err.message });
-                }
             } catch (err){
                 res.status(400).send({ error: err.message })
             }
         });
+});
+
+router.delete("/:id", (req, res) => {
+    fs.readFile(fileName, "utf8", (err, data) =>{
+        try{
+            if(err) throw err;
+            let json = JSON.parse(data);
+            const accounts = json.accounts.filter(account => account.id !== parseInt(req.params.id));
+            json.accounts = accounts;
+
+            fs.writeFile(fileName, JSON.stringify(json), err => {
+                if (err) {
+                    console.log(err);
+                }else {
+                    res.end();
+                }
+            });
+
+        } catch (err){
+            res.status(400).send({ error: err.message })
+        }
+    });
 });
 
 
